7d79eb37a037d3a37dd2a329f7f1a249424e4b38
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-10-09 20:30:10 -0700

    Interview Question answers Completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -14,20 +14,24 @@ class CuboidMaker {
         this.height = data.height;
     }
 
+    /* == Step 2: Volume Method ==
+      Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
+      
+      Formula for cuboid volume: length * width * height
+    */
     volume() {
         return this.length * this.width * this.height;
     }
+
+    /* == Step 3: Surface Area Method ==
+      Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
+      Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
+    */
     surfaceArea() {
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
 
 }
-
-
-/* == Step 3: Surface Area Method ==
-  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
-  Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
-*/
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
